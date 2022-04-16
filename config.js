97f5394c6af963907bd344208d7e2f954474b3df
Hassan Khan <40581532+hasssanhere@users.noreply.github.com>
2022-04-16 17:39:59 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+const blockfrostApiKey = {
+  0: "testnetUknOtJbpnomZdK1Ze6FkufzJfD4HKnJl", // testnet
+  1: "mainnetfqH0CVlBesnsj5IKhgIYCn231KzqUOyk", // mainnet
+};
+
+export default blockfrostApiKey;
